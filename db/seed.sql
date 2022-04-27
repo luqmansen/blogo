b935029a3b4515f64968a0f2c3bdff4418d788de
Luqman Setyo Nugroho <luqmansen@gmail.com>
2022-04-27 23:56:00 +0700

    update seed script to restart sequence after seeding
    update primary key migration for table

--- db/seed.sql
+++ db/seed.sql
@@ -3,7 +3,16 @@ values  (1, 'luqmansen', '1230j()U)GJIOPA', '2022-04-25 14:46:24.960559', '2022-
         (2, 'yeeterBoi', '1230j()U)GJIOPA', '2022-04-25 14:46:24.960559', '2022-04-25 14:46:24.960559'),
         (3, 'noggos', '1230j()U)GJIOPA', '2022-04-25 14:46:24.960559', '2022-04-25 14:46:24.960559');
 
-INSERT INTO public.posts (id, author_id, title, content, created_at, updated_at) VALUES (1, 1, 'Test', 'Teest Content', '2022-04-25 14:49:16.633220', '2022-04-25 14:49:16.633220');
+SELECT setval('public."users_id_seq"',
+              (SELECT MAX(id) FROM public.users)
+           );
+
+INSERT INTO public.posts (id, author_id, title, content, created_at, updated_at)
+VALUES (1, 1, 'Test', 'Teest Content', '2022-04-25 14:49:16.633220', '2022-04-25 14:49:16.633220');
+
+SELECT setval('public."posts_id_seq"',
+              (SELECT MAX(id) FROM public.posts)
+           );
 
 insert into public.comments (id, parent_post_id, parent_id, author_id, content, created_at, updated_at)
 values  (1, 1, null, 1, 'woii', '2022-04-25 14:49:51.379350', '2022-04-25 14:49:51.379350'),
@@ -13,6 +22,10 @@ values  (1, 1, null, 1, 'woii', '2022-04-25 14:49:51.379350', '2022-04-25 14:49:
         (5, 1, 4, 1, 'child again', '2022-04-25 14:50:49.071757', '2022-04-25 14:50:49.071757'),
         (6, 1, 5, 1, 'child nested again', '2022-04-25 14:50:56.528112', '2022-04-25 14:50:56.528112'),
         (7, 1, null, 1, 'child level 1 again', '2022-04-25 14:51:13.483050', '2022-04-25 14:51:13.483050');
+
+SELECT setval('public."comments_id_seq"',
+              (SELECT MAX(id) FROM public.comments)
+           );
 
 insert into public.reacts (id, name, thumbnail_url, created_at, updated_at)
 values  (1, 'like', 'http://example.com/like.jpg', '2022-04-25 14:56:12.047897', '2022-04-25 14:56:12.047897'),
